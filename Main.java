dcdb332d5f0ea601baa35a8feadd031d3b89a694
Robocontest yechimlari <99067087+Robocontest-yechimlari@users.noreply.github.com>
2024-03-15 16:02:33 +0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,26 +1,34 @@
 public class Main{
     public static void main(String[] args) {
+// int qiymat berilganida ishlaydigan methodlar
         Uchburchak uchburchak = new Uchburchak();
         uchburchak.setA(4);
         uchburchak.setB(3);
         uchburchak.setC(5);
-
+// double qiymat berilganida ishlaydigan methodlar
         Uchburchak duchburchak = new Uchburchak();
-        duchburchak.setA(4.0);
-        duchburchak.setB(3.0);
-        duchburchak.setC(5.0);
-
+        duchburchak.setAd(4000000.0);
+        duchburchak.setBd(3000000.0);
+        duchburchak.setCd(5000000.0);
+// long qiymat berilganida ishlaydigan methodlar
         Uchburchak luchburchak = new Uchburchak();
-        luchburchak.setA(40000);
-        luchburchak.setB(30000);
-        luchburchak.setC(50000);
-
+        luchburchak.setAl(40000);
+        luchburchak.setBl(30000);
+        luchburchak.setCl(50000);
+// float qiymat berilganida ishlaydigan methodlar
         Uchburchak fuchburchak = new Uchburchak();
-        fuchburchak.setA(4.0);
-        fuchburchak.setB(3.0);
-        fuchburchak.setC(5.0);
+        fuchburchak.setAf(4.0f);
+        fuchburchak.setBf(3.0f);
+        fuchburchak.setCf(5.0f);
 
         System.out.println("Uchburchak perimetri: "+uchburchak.getA());
         System.out.println("Uchburchak yuzasi: "+uchburchak.getB());
+        System.out.println("Uchburchak perimetri: "+duchburchak.getAD());
+        System.out.println("Uchburchak yuzasi: "+duchburchak.getBD());
+        System.out.println("Uchburchak perimetri: "+luchburchak.getAl());
+        System.out.println("Uchburchak yuzasi: "+luchburchak.getBl());
+        System.out.println("Uchburchak perimetri: "+fuchburchak.getAF());
+        System.out.println("Uchburchak yuzasi: "+fuchburchak.getBF());
+
     }
-        }
+}
